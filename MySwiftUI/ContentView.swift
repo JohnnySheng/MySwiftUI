44fb53715e63d5c04279576f67570ae88aa21704
Martin <yuangang.sheng@itc-engineering.com>
2020-02-21 08:58:36 +0100

    Initial Commit

---
+++ MySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUI
+//
+//  Created by Yuangang Sheng on 21.02.20.
+//  Copyright © 2020 Yuangang Sheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
